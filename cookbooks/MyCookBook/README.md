5d6de39e129e1c38a802b5880adcb288268fa29f
Tejash Khodiyar <Tejash.Khodiyar@247.ai>
2018-09-19 16:00:02 +0530

    Import MyCookBook version 0.1.0

--- cookbooks/MyCookBook/README.md
+++ cookbooks/MyCookBook/README.md
@@ -1,4 +1,4 @@
-# MyCookBook
+# mycookbook
 
 TODO: Enter the cookbook description here.
 
--- cookbooks/MyCookBook/recipes/default.rb
+++ cookbooks/MyCookBook/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook:: MyCookBook
+# Cookbook:: mycookbook
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright:: 2017, The Authors, All Rights Reserved.
